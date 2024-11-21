ce3250ef652592c98ead6baea39fb91914724ace
bmadaiah <bmadaiah@outlook.com>
2024-11-21 14:09:30 +1300

    Update

--- Day-5/main.tf
+++ Day-5/main.tf
@@ -47,8 +47,8 @@ resource "aws_security_group" "webSg" {
 
   ingress {
     description = "HTTP from VPC"
-    from_port   = 80
-    to_port     = 80
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
